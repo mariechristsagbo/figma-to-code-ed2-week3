3bf4ddf277ffda5efef38ef2b6f48d7d28d6a40a
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-08-19 15:52:48 +0100

    - remove Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
 
@@ -8,7 +7,6 @@ const mona = localFont({
   variable: "--font-mona",
 });
     
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
