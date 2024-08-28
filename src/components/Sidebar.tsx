a9eaddd691d329bc5b00ed5fd47c4c18d61c2c06
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-08-28 15:22:49 +0100

    - update style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar, closeSidebar }
   return (
     <>
       <aside
-        className={`fixed top-0 left-0 w-72 h-full sm:h-auto dark:bg-tokena-dark-blue-1 border-r dark:border-tokena-dark-gray dark:border-opacity-40 transform lg:static bg-white ${
+        className={`fixed top-0 left-0 w-72 h-full lg:h-auto dark:bg-tokena-dark-blue-1 border-r dark:border-tokena-dark-gray dark:border-opacity-40 transform lg:static bg-white ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         } transition-transform duration-300 ease-in-out lg:translate-x-0 flex flex-col font-mona font-medium px-4`}
       >
