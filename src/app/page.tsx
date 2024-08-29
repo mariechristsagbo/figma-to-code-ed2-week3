eba76bf56e35bf9e9d79f11102a37400db55c106
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-08-29 01:10:26 +0100

    - update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+'use client'
+import { useState } from "react";
 import Sidebar from "@/components/Sidebar"
 import DashboardView from "@/components/DashboardView"
 
 export default function Dashboard() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
   return (
     <div className="flex dark:bg-tokena-dark-blue-1 dark:text-tokena-gray min-h-screen w-full">
-      <Sidebar/>
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        closeSidebar={closeSidebar}
+      />
       <DashboardView/>
     </div>
   )
-}
+} }
