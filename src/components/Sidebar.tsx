059282a033c78701a2084733d0541ceb793c47e4
mariechristsagbo <christsagbo1@gmail.com>
2024-10-16 15:35:08 +0100

    - format

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,14 +10,13 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar, closeSidebar }) => {
   const router = useRouter();
-  const pathname = usePathname(); 
+  const pathname = usePathname();
 
   return (
     <>
       <aside
-        className={`fixed top-0 left-0 h-full w-[250px] lg:h-auto dark:bg-tokena-dark-blue-1 border-r dark:border-tokena-dark-gray dark:border-opacity-40 transform lg:static bg-white ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out lg:translate-x-0 flex flex-col font-mona font-medium px-3`}
+        className={`fixed top-0 left-0 h-full w-[250px] lg:h-auto dark:bg-tokena-dark-blue-1 border-r dark:border-tokena-dark-gray dark:border-opacity-40 transform lg:static bg-white ${isOpen ? 'translate-x-0' : '-translate-x-full'
+          } transition-transform duration-300 ease-in-out lg:translate-x-0 flex flex-col font-mona font-medium px-3`}
       >
         <div className="my-5 py-3 bg-tokena-blue bg-opacity-5 rounded-xl px-3 cursor-pointer" onClick={() => router.push("/")}>
           <div className="flex items-center space-x-2">
@@ -39,11 +38,10 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar, closeSidebar }
                 <li key={link.label} onClick={() => router.push(link.path)}>
                   <a
                     href="#"
-                    className={`flex items-center p-3 ${
-                      isActive
+                    className={`flex items-center p-3 ${isActive
                         ? "text-tokena-white bg-tokena-blue"
                         : "text-tokena-dark dark:text-tokena-gray dark:hover:bg-tokena-blue hover:bg-gray-100"
-                    } rounded-xl`}
+                      } rounded-xl`}
                   >
                     <img
                       src={link.icon}
